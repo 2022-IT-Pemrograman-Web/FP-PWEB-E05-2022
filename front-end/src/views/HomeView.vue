f8dcf4554f3c6f63d5543f99dde6e2bee22fde94
florentinobenedictus <florentinobenedictus@gmail.com>
2022-12-15 09:59:03 +0700

    connect register to front-end

--- front-end/src/views/HomeView.vue
+++ front-end/src/views/HomeView.vue
@@ -1,18 +1,41 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div v-if="response != 0">{{ this.response }}</div>
+    <form id="register" @submit="registerUser">
+      <input type="text" id="inputUN" placeholder="username" required />
+      <input type="text" id="inputPW" placeholder="password" required />
+      <input type="submit" Value="Register" />
+    </form>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import { server } from "../utils/helper";
+import axios from "axios";
 
 export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      response: 0,
+    };
+  },
+  mounted() {},
+  methods: {
+    registerUser(e) {
+      e.preventDefault();
+      const inputUN = document.getElementById("inputUN").value;
+      const inputPW = document.getElementById("inputPW").value;
+
+      const user = {
+        username: inputUN,
+        password: inputPW,
+      };
+      axios
+        .post(`${server.baseURL}/register`, user)
+        .then((response) => (this.response = response.data))
+        .catch((error) => (this.response = error.response.data.message));
+      console.log(this.response);
+    },
   },
 };
 </script>
